cfdfde937b493e7db9e11a08b0de860fb8695845
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2026-02-23 16:45:54 +0900

    fix bug

--- js/controllers/AppController.js
+++ js/controllers/AppController.js
@@ -345,7 +345,7 @@ export class AppController {
   async registerServiceWorker() {
     if ('serviceWorker' in navigator) {
       try {
-        const registration = await navigator.serviceWorker.register('/sw.js');
+        const registration = await navigator.serviceWorker.register('./sw.js');
         console.log('Service Worker registered:', registration);
       } catch (error) {
         console.error('Service Worker registration failed:', error);
